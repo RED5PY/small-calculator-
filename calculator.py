b02766872e7459457f89a88d38c033f55e41698c
H4CK6R SPY <71980380+vjkingler@users.noreply.github.com>
2021-03-11 18:12:39 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,69 @@
+
+print("")
+print(" ________________UNIF CALCULATOR÷_______________v1.0")
+print("")
+print("")
+print("<××××××××××××××××××××××××××××××××××××××××××××××>")
+print("              Created by SPY ")
+print("<××××××××××××××××××××××××××××××××××××××××××××××>")
+print("")
+print("")
+print("Choose your choice")
+print("")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+print("5.About")
+print("6.exit")
+print("")
+ch=int(input("enter your choice : "))
+
+if ch==1:
+	
+	a=int(input("enter 1st number : "))
+	b=int(input("enter 2nd number : "))
+	c=a+b
+	print("     ans = ",c)
+	
+	
+if ch==2:
+	
+	a=int(input("enter 1st number : "))
+	b=int(input("enter 2nd number : "))
+	c=a-b
+	print("     ans = ",c)
+
+if ch==3:
+	
+	a=int(input("enter 1st number : "))
+	b=int(input("enter 2nd number : "))
+	c=a*b
+	print("     ans = ",c) 
+	
+if ch==4:
+	
+	a=int(input("enter 1st number : "))
+	b=int(input("enter 2nd number : "))
+	c=a/b
+	print("     ans = ",c) 
+	
+if ch==5:
+	print("")
+	print("This is a simple claculator work both on kali linux and termux.")
+	print("")
+	print("v1.0")
+
+	print("")
+	print("                    Created by SPY")
+	
+	print("")
+	print("<=======================================================>")
+	
+	print("")
+	print("                 Thank you for using..")
+	
+if ch==6:
+	exit
+
+
